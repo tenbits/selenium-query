286fed846701c746f0913b9781c4dc00daf0d354
Alex Kit <source.kit@gmail.com>
2018-09-22 01:07:41 +0200

    v0.51.41

--- src/scripts/nodeProperty.es6
+++
@@ -1,9 +0,0 @@
-function scripts_nodeProperty (){
-	// import ./_inlineGetSetKeyValue.js
-	function get(el, key) {
-		return el[key];
-	}
-	function set(el, key, val) {
-		el[key] = val;
-	}
-}
--- tools/build-dts.js
+++
@@ -1,9 +0,0 @@
-var dts = require('dts-bundle');
-
-dts.bundle({
-	name: 'selenium-query',
-	main: './ts-temp/SQueryLibrary.d.ts',
-	out: './typings/index.d.ts'
-});
-
-io.File.copyTo('./ts-temp/typings/index.d.ts', './lib/query.d.ts');
