d42da2c2b8da672f4faaedd5486a53a2eceaf177
Alex Kit <source.kit@gmail.com>
2018-10-03 12:57:40 +0200

    v0.51.52

--- src/SQueryLibrary.ts
+++
@@ -1,9 +0,0 @@
-import { WebdriverQuery } from './webdriver/WebdriverQuery'
-import { Classify, FnPrototypeAlias } from './utils/classify';
-
-@Classify
-@FnPrototypeAlias
-class SQuery extends WebdriverQuery {
-
-}
-export { SQuery }
--- src/webdriver/scripts/class/has.es6
+++
@@ -1,7 +0,0 @@
-function scripts_nodeClassHas () {
-	// import ./_inline.js
-	if (el.classList) {
-		return el.classList.contains(klass)
-	}
-	return -1 !== (' ' + el.className + ' ').indexOf(' ' + klass + ' ');
-}
